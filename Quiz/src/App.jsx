1fff5001c00409493a1240956c865c6ded7c631a
Caio Ricardo <contatocaioobr@hotmail.com>
2023-05-03 16:07:17 -0300

    final

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,24 +1,26 @@
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
 
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
 
-import Welcome from './components/welcome';
-import Question from './components/question'
-import { QuizContext } from './context/Quiz';
-import './App.css';
-import { useContext } from 'react';
+import PickCategory from "./components/PickCategory";
 
-
-
+import "./App.css";
 
 function App() {
-  const [quizState, dispath] = useContext(QuizContext)
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div className="App">
-      <h1>Quiz sobre Programacão</h1>
-      {quizState.gameStage === "Start" && <Welcome/>}
-      {quizState.gameStage === "Playing" && <Question/>}
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Quiz/src/components/Welcome.jsx
+++ Quiz/src/components/Welcome.jsx
@@ -1,22 +1,23 @@
-import React, { Component } from 'react'
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import "./Welcome.css";
+
 import Quiz from "../img/quiz.svg";
-import "./Welcome.css"
-import { useContext } from 'react';
-import { QuizContext } from '../context/quiz';
 
+const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
 
-const Welcome = () => { 
-  const [quizState, dispath] = useContext(QuizContext)
-  
-    
-    return (
-      <div id='Welcome'>
-        <h2>Bem vindo</h2>
-        <p>Clique no botao para iniciar</p>
-      <button onClick={()=> dispath({type: "CHANGE_STATE"})}>Iniciar</button>
-        <img src={Quiz} alt="Logo inico" />
-      </div>
-    )
-  }
+  return (
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Início do Quiz" />
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
